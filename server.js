a33dbdd8fc881138257ff66f669414a1d9d14d62
marcod0410 <dlsmarco0410@gmail.com>
2021-11-12 13:59:27 -0500

    checkup

--- server.js
+++ server.js
@@ -10,10 +10,100 @@ const { application } = require('express')
 const app = express()
 const {PORT = 3000} = process.env
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
+
+/////////////////////////////////
+// Database Connection
+////////////////////////////////
+// establish connection
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`)
+// Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to Mongo"))
+.on("close", () => console.log("You are disconnected from Mongo"))
+.on("error", (error) => console.log(error))
+
+
+//////////////////////////////
+// Models
+//////////////////////////////
+
+
+const BookmarkSchema = new mongoose.Schema({
+    title: String,
+    url: String 
+}, )
+
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema)
+
+
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+
+
+
+// Routes
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
 })
+
+//  INDEX ROUTE
+app.get("/bookmarks", async (req, res) => {
+    try {
+      res.json(await Bookmark.find({}));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+
+
+// CREATE ROUTE 
+app.post("/bookmarks", async (req, res) => {
+    try {
+      res.json(await Bookmark.create(req.body));
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  });
+
+
+  // UPDTAE ROUTE 
+app.put("/bookmarks/:id", async (req, res) => {
+    try {
+       
+        res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+      } catch (error) {
+        res.status(400).json({ error });
+      }
+})
+
+
+// DELETE ROUTE 
+app.delete("/bookmarks/:id", async (req, res) => {
+    try {
+   
+      res.json(await Bookmark.findByIdAndRemove(req.params.id));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+
+
+/////////////////////////////////
+// Server Listener
+/////////////////////////////////
+app.listen(PORT, () => {console.log(`listening on PORT ${PORT}`)})
